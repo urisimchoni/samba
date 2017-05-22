99250cb7f0070f4908a6a64a4ec29933fc70b979
Uri Simchoni <uri@ctera.com>
2017-05-22 12:20:44 +0000

    winbindd: reinitialize id-mapping child on fork
    When the id-mapping child is forked, it needs to
    know the SID of all domains. Do this when it is
    restarted as well.
    
    Fixes APP-13427

--- source3/winbindd/winbindd_idmap.c
+++ source3/winbindd/winbindd_idmap.c
@@ -27,7 +27,49 @@
 #undef DBGC_CLASS
 #define DBGC_CLASS DBGC_WINBIND
 
+static void init_idmap_process_done(struct tevent_req *req);
 static struct winbindd_child static_idmap_child;
+
+static void idmap_child_on_process_init(struct winbindd_child *child)
+{
+	struct winbindd_domain *domain;
+	struct tevent_req *req;
+
+	/* initialize all domains we already know of.
+	 * newly-added domains will be initialized
+	 * as they are added
+	 */
+	for (domain = domain_list(); domain != NULL; domain = domain->next) {
+		req =
+		    wb_init_idmap_backend_send(domain, winbind_event_context(),
+					       domain->name, &domain->sid);
+		if (!req) {
+			smb_panic(
+			    "failed calling idmap child to initialize backend");
+		}
+		tevent_req_set_callback(req, init_idmap_process_done, domain);
+	}
+}
+
+static void init_idmap_process_done(struct tevent_req *req)
+{
+	struct winbindd_domain *domain =
+	    tevent_req_callback_data(req, struct winbindd_domain);
+	NTSTATUS status;
+	bool require_sid_type = true;
+
+	status = wb_init_idmap_backend_recv(req, &require_sid_type);
+	TALLOC_FREE(req);
+	if (!NT_STATUS_IS_OK(status)) {
+		DBG_WARNING("Failed initializing idmap for domain %s\n",
+			    domain->name);
+		return;
+	}
+
+	DBG_DEBUG("Domain %s id-mapping %s sid type\n", domain->name,
+		  require_sid_type ? "requires" : "does not require");
+	domain->idmap_require_sid_type = require_sid_type;
+}
 
 struct winbindd_child *idmap_child(void)
 {
@@ -52,6 +94,6 @@ void init_idmap_child(void)
 {
 	setup_child(NULL, &static_idmap_child,
 		    idmap_dispatch_table,
-		    NULL,
+		    idmap_child_on_process_init,
 		    "log.winbindd", "idmap");
 }
